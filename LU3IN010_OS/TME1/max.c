ecbc99695f460b13837637d8596a18d7181318a2
Ichraq <146119083+Ichraq05@users.noreply.github.com>
2025-01-24 15:47:51 +0000

    ---

---
+++ LU3IN010_OS/TME1/max.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+int max_list(char **list){
+    int i = 1;
+    int res = atoi(list[1]);
+
+    while (list[i] != NULL){
+        if (res < atoi(list[i]))
+        {
+            res = atoi(list[i]);
+        }
+        i++;
+    }
+    return res;
+}
+
+
+
+int main(int argc, char **argv){
+    if(argc < 2){
+        printf("Entrer au moins un nombre.");
+        return 1;
+    }
+
+    int max = max_list(argv);
+    printf("%d \n", max);
+    return 0;
+}
+
